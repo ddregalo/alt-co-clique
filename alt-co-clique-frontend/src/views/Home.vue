f91137d16f77019485a2c19e5d8db8554254c8c1
danielcampos <ddregalo@gmail.com>
2019-05-19 13:10:30 +0100

    Add banner image with title

--- alt-co-clique-frontend/src/views/Home.vue
+++ alt-co-clique-frontend/src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <b-img src="http://www.luapstudios.co.uk/sites/default/files/artwork/KK47_karen_el_khazen_guns_water_tank_elle_luap.JPG" fluid alt="Responsive image" id="landing-image" class="mw-100"></b-img>
+    <div class="banner-title">SITE COMING SOON</div>
   </div>
 </template>
 
@@ -12,3 +14,22 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+  .banner-title {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 3em;
+    font-family: Helvetica;
+    color: rgb(240, 240, 240);
+    font-weight: lighter;
+    letter-spacing: 10px;
+  }
+  
+  #landing-image {
+    height: 20%;
+    width: 100%;
+  }
+</style>
