ed33b9b7fd5294bd4c3c1a90c0c59d5cc6c17cfa
danielcampos <ddregalo@gmail.com>
2019-05-19 17:26:23 +0100

    Import main logos + use in transparent sticky navbar

--- alt-co-clique-frontend/src/components/NavBar.vue
+++ alt-co-clique-frontend/src/components/NavBar.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" id="nav-bar">
-      <b-navbar-brand href="/" id="nav-logo">ALT-CO CLIQUE</b-navbar-brand>
+    <b-navbar id="nav-bar" fixed="top">
+      <b-navbar-brand href="/">
+        <img src="../assets/white.svg" id="logo"/>
+      </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <router-link :to="'contact'" id="first-nav-link">contact</router-link>
+          
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
+          <router-link :to="'contact'" id="last-nav-link">contact</router-link>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -27,19 +30,21 @@ export default Vue.extend({
 </script>
 
 <style scoped lang="scss">
-  #first-nav-link {
-    margin-left: 50px;
+  #last-nav-link {
+    margin-right: 10em;
     text-decoration: none;
-    color: rgb(126, 126, 126);
+    color: rgb(245, 245, 245);
     text-transform: uppercase;
-    font-size: 0.9em;
+    font-size: 1.1em;
+    font-family: 'Ropa Sans', sans-serif;
   }
 
   #nav-bar {
-    background-color: rgb(243, 243, 243)
+    background: transparent;
   }
 
-  #nav-logo {
+  #logo {
     margin-left: 4em;
+    max-height: 4em;
   }
 </style>
