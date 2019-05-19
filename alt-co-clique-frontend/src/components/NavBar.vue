6782ac667ef286edfc96acc39c17fabe05d2af23
danielcampos <ddregalo@gmail.com>
2019-05-19 11:05:05 +0100

    Basic NavBar formatting + styling

--- alt-co-clique-frontend/src/components/NavBar.vue
+++ alt-co-clique-frontend/src/components/NavBar.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand href="#">ALT-CO CLIQUE</b-navbar-brand>
+    <b-navbar toggleable="lg" id="nav-bar">
+      <b-navbar-brand href="#" id="nav-logo">ALT-CO CLIQUE</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <router-link :to="'about'" id="first-nav-link">contact</router-link>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
@@ -31,18 +30,19 @@ export default Vue.extend({
 </script>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  #first-nav-link {
+    margin-left: 50px;
+    text-decoration: none;
+    color: rgb(126, 126, 126);
+    text-transform: uppercase;
+    font-size: 0.9em;
+  }
+
+  #nav-bar {
+    background-color: rgb(243, 243, 243)
+  }
+
+  #nav-logo {
+    margin-left: 4em;
+  }
 </style>
